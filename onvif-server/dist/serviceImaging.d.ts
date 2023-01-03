1bc68295707d9cfe1e63433f79bca3d4368b1b43
nanosonde <2073569+nanosonde@users.noreply.github.com>
2023-01-03 17:19:27 +0100

    Remove node_modules and dist

--- onvif-server/dist/serviceImaging.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnvifServer } from "./serverMockup";
-export declare class ImagingService {
-    readonly serviceName = "imaging";
-    constructor(server: OnvifServer);
-    private getImagingSettings;
-    private getMoveOptions;
-    private getOptions;
-    private setImagingSettings;
-}
--- onvif-server/dist/servicePtz.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnvifServer } from "./serverMockup";
-export declare class PtzService {
-    readonly serviceName = "ptz";
-    constructor(server: OnvifServer);
-    private getPresets;
-    private getNodes;
-    private gotoPreset;
-    private gotoHomePosition;
-}
