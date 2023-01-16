c57af115749480018c5ec737bc0689c907ea8842
nanosonde <2073569+nanosonde@users.noreply.github.com>
2023-01-16 15:29:33 +0100

    clean up

--- docker/rootfs/etc/cont-init.d/setup-pulseaudio.sh
+++ docker/rootfs/etc/cont-init.d/setup-pulseaudio.sh
@@ -10,12 +10,21 @@ adduser --home /usr/local/pulseaudio/var/run/pulse --disabled-password --disable
 && adduser pulse pulse-access \
 && adduser root pulse \
 && adduser root pulse-access \
+echo "Done.";
 
+echo "Setting up pulseaudio runtime path";
 mkdir -p /usr/local/pulseaudio/var/lib/pulse
 chown pulse.pulse /usr/local/pulseaudio/var/lib/pulse
 chmod 644 /usr/local/pulseaudio/var/lib/pulse
-
 echo "Done.";
 
-# Overwrite the default config files with ours
-cp -av /etc/pulse /usr/local/pulseaudio/etc/
+echo "Modifying pulseaudio client config";
+echo "autospawn = no" >> /usr/local/pulseaudio/etc/pulse/client.conf
+echo "default-server = unix:/tmp/pulseaudio.socket" >> /usr/local/pulseaudio/etc/pulse/client.conf
+echo "enable-shm = no" >> /usr/local/pulseaudio/etc/pulse/client.conf
+echo "daemon-binary = /bin/true" >> /usr/local/pulseaudio/etc/pulse/client.conf
+echo "Done.";
+
+echo "Overwriting the default system mode script";
+cp -av /etc/pulse/system.pa /usr/local/pulseaudio/etc/pulse/system.pa
+echo "Done.";
