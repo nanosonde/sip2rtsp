5bb4515e06da16605f1b2ac4c13ca2615d88fdb8
nanosonde <2073569+nanosonde@users.noreply.github.com>
2023-01-16 15:43:31 +0100

    Fix pulseaudio setup

--- docker/rootfs/etc/cont-init.d/setup-pulseaudio.sh
+++ docker/rootfs/etc/cont-init.d/setup-pulseaudio.sh
@@ -5,11 +5,11 @@
 set -o errexit -o nounset -o pipefail
 
 echo "Creating user pulse and group pulse-access";
-adduser --home /usr/local/pulseaudio/var/run/pulse --disabled-password --disabled-login --gecos "" pulse \
+adduser --home /usr/local/pulseaudio/var/run/pulse --disabled-password --disabled-login --gecos '' pulse \
 && addgroup pulse-access \
 && adduser pulse pulse-access \
 && adduser root pulse \
-&& adduser root pulse-access \
+&& adduser root pulse-access
 echo "Done.";
 
 echo "Setting up pulseaudio runtime path";
@@ -19,10 +19,11 @@ chmod 644 /usr/local/pulseaudio/var/lib/pulse
 echo "Done.";
 
 echo "Modifying pulseaudio client config";
-echo "autospawn = no" >> /usr/local/pulseaudio/etc/pulse/client.conf
-echo "default-server = unix:/tmp/pulseaudio.socket" >> /usr/local/pulseaudio/etc/pulse/client.conf
-echo "enable-shm = no" >> /usr/local/pulseaudio/etc/pulse/client.conf
-echo "daemon-binary = /bin/true" >> /usr/local/pulseaudio/etc/pulse/client.conf
+cp -av /usr/local/pulseaudio/etc/pulse/client.conf /etc/pulse/client.conf
+echo "autospawn = no" >> /etc/pulse/client.conf
+echo "default-server = unix:/tmp/pulseaudio.socket" >> /etc/pulse/client.conf
+echo "enable-shm = no" >> /etc/pulse/client.conf
+echo "daemon-binary = /bin/true" >> /etc/pulse/client.conf
 echo "Done.";
 
 echo "Overwriting the default system mode script";
