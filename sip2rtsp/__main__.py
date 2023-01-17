3ba9a341ff9094d3f2fdf22e5f8885c39ef28014
nanosonde <2073569+nanosonde@users.noreply.github.com>
2023-01-17 11:20:33 +0100

    save work

--- sip2rtsp/__main__.py
+++ sip2rtsp/__main__.py
@@ -1,15 +1,42 @@
 import faulthandler
 
 faulthandler.enable()
+from sip2rtsp.gi import GLib  # noqa: F401
+
 import threading
+import signal
+from typing import Optional
+from types import FrameType
+import logging
+import time
 
 threading.current_thread().name = "sip2rtsp"
 
-from sip2rtsp.gi import Gst  # noqa: F401
-
 from sip2rtsp.app import Sip2RtspApp
 
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
-    sip2rtsp_app = Sip2RtspApp()
+    logging.basicConfig(level=logging.DEBUG)
+
+    loop = GLib.MainLoop()
+
+    thread = threading.Thread(target=loop.run)
+
+    sip2rtsp_app = Sip2RtspApp(loop)
+
+    def receiveSignal(signalNumber: int, frame: Optional[FrameType]) -> None:
+        logger.info(f"Received SIGINT...")
+        sip2rtsp_app.stop()
+        logger.info(f"Quitting glib main loop...")
+        loop.quit()
+        logger.info(f"Done.")
+        logger.info(f"Waiting for glib thread to join...")
+        thread.join()
+        logger.info(f"Done.")
+
+    signal.signal(signal.SIGINT, receiveSignal)
 
     sip2rtsp_app.start()
+
+    thread.start()
--- sip2rtsp/app.py
+++ sip2rtsp/app.py
@@ -1,15 +1,8 @@
 import logging
-import multiprocessing as mp
-from multiprocessing.queues import Queue
-import signal
-import sys
-from typing import Optional
-from types import FrameType
 
 from sip2rtsp.version import VERSION
-from sip2rtsp.log import log_process, root_configurer
 
-from .gi import GLib, Gst, GstRtspServer
+from .gi import GstRtspServer
 
 logger = logging.getLogger(__name__)
 
@@ -20,58 +13,12 @@ launch_string = f'souphttpsrc location=http://10.10.10.10:54321/stream is-live=t
 # launch_string = f"souphttpsrc location=http://10.10.10.10:54321/stream is-live=true do-timestamp=false timeout=5 ! multipartdemux ! jpegdec ! videoconvert ! videoscale ! videorate ! clockoverlay ! video/x-raw,format=I420,width=1280,height=720,framerate=15/1 ! x264enc speed-preset=superfast bitrate=2000 option-string=keyint=60:min-keyint=60 key-int-max=60 ! rtph264pay config-interval=-1 name=pay0 pt=96 pulsesrc device=\"BaresipSpeaker.monitor\" ! audio/x-raw,format=S16LE,channels=1,rate=8000 ! audioconvert ! audioresample ! audio/x-raw,format=S16LE,channels=1,rate=8000 !  mulawenc ! rtppcmupay name=pay1"
 
 
-def media_prepared(media):
-    print(
-        "media_prepared(): media: {media}, status: {status}".format(
-            media=str(media), status=media.get_status()
-        ),
-        flush=True,
-    )
-
-    n_streams = media.n_streams()
-    print(
-        "media_configure(): number of streams: {num}".format(num=n_streams), flush=True
-    )
-    for n in range(n_streams):
-        print(
-            "stream {n}: media_configure(): ssrc: {ssrc}".format(
-                n=n, ssrc=media.get_stream(n).get_ssrc()
-            ),
-            flush=True,
-        )
-        Gst.debug_bin_to_dot_file(
-            media.get_stream(n).get_joined_bin(),
-            Gst.DebugGraphDetails.ALL,
-            "stream" + str(n),
-        )
-        # media.get_stream(n).set_blocked(False)
-
-
-def media_configure(factory, media):
-    print(
-        "media_configure(): media: {media}, status: {status}".format(
-            media=str(media), status=media.get_status()
-        ),
-        flush=True,
-    )
-    media.connect("prepared", media_prepared)
-    Gst.debug_bin_to_dot_file(
-        media.get_element(), Gst.DebugGraphDetails.ALL, "media-configure"
-    )
-    n_streams = media.n_streams()
-    print(
-        "media_configure(): number of streams: {num}".format(num=n_streams), flush=True
-    )
-
-
 class Sip2RtspApp:
-    def __init__(self) -> None:
-        self.log_queue: Queue = mp.Queue()
-        self.loop = GLib.MainLoop()
+    def __init__(self, loop) -> None:
+        self.loop = loop
         self.server = GstRtspServer.RTSPOnvifServer.new()
         self.factory = GstRtspServer.RTSPOnvifMediaFactory.new()
         self.factory.set_media_gtype(GstRtspServer.RTSPOnvifMedia)
-        self.factory.set_launch(launch_string)
         # self.factory.set_backchannel_launch("( capsfilter caps=\"application/x-rtp,media=audio,payload=0,clock-rate=8000,encoding-name=PCMU\" name=depay_backchannel ! rtppcmudepay ! fakesink async=false )")
         # self.factory.set_backchannel_launch("( capsfilter caps=\"application/x-rtp,media=audio,payload=0,clock-rate=8000,encoding-name=PCMU\" name=depay_backchannel ! rtppcmudepay ! mulawdec ! wavenc ! filesink location=test.wav async=false )")
         self.factory.set_backchannel_launch(
@@ -83,35 +30,49 @@ class Sip2RtspApp:
         self.factory.set_enable_rtcp(False)
         # self.factory.set_protocols(GstRtsp.RTSPLowerTrans.TCP)
         # self.factory.set_profiles(GstRtsp.RTSPProfile.AVP)
-        self.factory.connect("media-configure", media_configure)
+        self.factory.connect("media-configure", self.media_configure)
         self.server.get_mount_points().add_factory("/test", self.factory)
 
-        self.server.attach()
-
-    def init_logger(self) -> None:
-        self.log_process = mp.Process(
-            target=log_process, args=(self.log_queue,), name="log_process"
-        )
-        self.log_process.daemon = True
-        self.log_process.start()
-        root_configurer(self.log_queue)
+        self.server.attach(self.loop.get_context())
 
     def start(self) -> None:
-        self.init_logger()
         logger.info(f"Starting SIP2RTSP ({VERSION})")
-
-        def receiveSignal(signalNumber: int, frame: Optional[FrameType]) -> None:
-            self.stop()
-
-        signal.signal(signal.SIGINT, receiveSignal)
-
-        try:
-            self.loop.run()
-        except KeyboardInterrupt:
-            pass
-
-        sys.exit()
+        self.factory.set_launch(launch_string)
 
     def stop(self) -> None:
         logger.info(f"Stopping...")
-        self.loop.quit()
+
+    def media_prepared(self, media):
+        logger.info(
+            "media_prepared(): media: {media}, status: {status}".format(
+                media=str(media), status=media.get_status()
+            )
+        )
+
+        n_streams = media.n_streams()
+        logger.info("media_prepared(): number of streams: {num}".format(num=n_streams))
+        for n in range(n_streams):
+            logger.info(
+                "media_prepared(): stream {n}: ssrc: {ssrc}".format(
+                    n=n, ssrc=media.get_stream(n).get_ssrc()
+                )
+            )
+            # Gst.debug_bin_to_dot_file(
+            #     media.get_stream(n).get_joined_bin(),
+            #     Gst.DebugGraphDetails.ALL,
+            #     "stream" + str(n),
+            # )
+            # media.get_stream(n).set_blocked(False)
+
+    def media_configure(self, factory, media):
+        logger.info(
+            "media_configure(): media: {media}, status: {status}".format(
+                media=str(media), status=media.get_status()
+            )
+        )
+        media.connect("prepared", self.media_prepared)
+        # Gst.debug_bin_to_dot_file(
+        #     media.get_element(), Gst.DebugGraphDetails.ALL, "media-configure"
+        # )
+        n_streams = media.n_streams()
+        logger.info("media_configure(): number of streams: {num}".format(num=n_streams))
