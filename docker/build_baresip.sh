1dc2a3c7e34ace9f0542f0e056b5ede47bc61a09
nanosonde <2073569+nanosonde@users.noreply.github.com>
2023-03-23 16:22:55 +0100

    Bump baresip version to 3.0.0

--- docker/build_baresip.sh
+++ docker/build_baresip.sh
@@ -2,9 +2,8 @@
 
 set -euxo pipefail
 
-LIBRE_VERSION=2.12.0
-LIBREM_VERSION=2.12.0
-BARESIP_VERSION=2.12.0
+LIBRE_VERSION=3.0.0
+BARESIP_VERSION=3.0.0
 
 echo "deb-src http://ftp.de.debian.org/debian/ bookworm main contrib non-free non-free-firmware" > /etc/apt/sources.list.d/sources-src.list
 apt-get update
@@ -18,17 +17,11 @@ apt-get -qq install -y \
 
 apt-get -qq build-dep -y baresip
     
-# Create a temporary directory for building libre, download the source code, extract it, and remove the archive
+# Create a temporary directory for building libre (librem is included since 3.0.0), download the source code, extract it, and remove the archive
 mkdir /tmp/libre
 wget https://github.com/baresip/re/archive/refs/tags/v${LIBRE_VERSION}.tar.gz
 tar -zxf v${LIBRE_VERSION}.tar.gz -C /tmp/libre --strip-components=1
 rm v${LIBRE_VERSION}.tar.gz
-
-# Create a temporary directory for building librem, download the source code, extract it, and remove the archive
-mkdir /tmp/librem
-wget https://github.com/baresip/rem/archive/refs/tags/v${LIBREM_VERSION}.tar.gz
-tar -zxf v${LIBREM_VERSION}.tar.gz -C /tmp/librem --strip-components=1
-rm v${LIBREM_VERSION}.tar.gz
 
 # Create a temporary directory for building baresip, download the source code, extract it, and remove the archive
 mkdir /tmp/baresip
@@ -38,20 +31,14 @@ rm v${BARESIP_VERSION}.tar.gz
 
 mkdir -p /usr/local/baresip
 
-# Build and install libre
+# Build and install libre (with librem)
 cd /tmp/libre
-cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr/
-cmake --build build -j
-cmake --install build --prefix dist && cp -a dist/* /usr/ && cp -a dist/* /usr/local/baresip/
-
-# Build and install librem
-cd /tmp/librem
-cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr/
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/
 cmake --build build -j
 cmake --install build --prefix dist && cp -a dist/* /usr/ && cp -a dist/* /usr/local/baresip/
 
 # Build and install baresip
 cd /tmp/baresip
-cmake -B build -DSTATIC=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr/
+cmake -B build -DSTATIC=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/
 cmake --build build -j
 cmake --install build --prefix dist && cp -a dist/* /usr/ && cp -a dist/* /usr/local/baresip/
